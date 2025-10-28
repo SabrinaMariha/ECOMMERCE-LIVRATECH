8ebee3362ebd4690c1d38d3d3ab6566df694512c
Daniel Andrade <dan.andrade313@gmail.com>
2025-10-27 21:07:33 -0300

    criado entity de SolicitacaoTroca

---
+++ src/main/java/com/sabrina/daniel/Livratech/model/SolicitaçãoTroca.java
@@ -0,0 +1,31 @@
+package com.sabrina.daniel.Livratech.model;
+
+import com.sabrina.daniel.Livratech.enums.StatusTroca;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class SolicitaçãoTroca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne @JoinColumn(name = "item_id")
+    private Item item;
+
+    private String motivo;
+    private LocalDateTime dataSolicitacao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusTroca status;
+}
