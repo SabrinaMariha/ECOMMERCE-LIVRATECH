5884fa977d8063a65f6eae03a468c38b9a5ddceb
Sabrina Mariha <141959258+SabrinaMariha@users.noreply.github.com>
2025-11-06 17:48:37 +0000

    feat: metodo de filtro de produtos no repository

--- src/main/java/com/sabrina/daniel/Livratech/daos/ProdutoRepository.java
+++ src/main/java/com/sabrina/daniel/Livratech/daos/ProdutoRepository.java
@@ -9,4 +9,20 @@ import java.util.List;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     List<Produto> findAllByOrderByIdAsc();
+    
+     @Query("""
+        SELECT p FROM Produto p
+        WHERE (:nome IS NULL OR LOWER(p.nome) LIKE LOWER(CONCAT('%', :nome, '%')))
+        AND (:autor IS NULL OR LOWER(p.autor) LIKE LOWER(CONCAT('%', :autor, '%')))
+        AND (:precoMin IS NULL OR p.preco >= :precoMin)
+        AND (:precoMax IS NULL OR p.preco <= :precoMax)
+        AND (:categorias IS NULL OR p.categoria IN :categorias)
+    """)
+    List<Produto> filtrarProdutos(
+            @Param("nome") String nome,
+            @Param("autor") String autor,
+            @Param("precoMin") Double precoMin,
+            @Param("precoMax") Double precoMax,
+            @Param("categorias") List<Categoria> categorias
+    );
 }
